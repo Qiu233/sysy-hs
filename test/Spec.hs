0204be26f1ce150c4cb14aa06e8ecdf294ad3e81
Qiu233 <lopi258@outlook.com>
2024-05-09 17:22:50 +0800

    chore: make use of hspec

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,11 +3,12 @@
 import Test.Hspec
 import System.IO
 import System.Directory (listDirectory)
-import Data.Functor (void)
 import Data.List (sort, isSuffixOf)
-import Control.Monad (forM_)
+import Control.Monad (forM_, forM)
 import Text.Parsec (runParser)
 import SysY.Parser
+import Data.Either (isRight)
+import Control.DeepSeq (($!!))
 
 func_test_path :: String
 func_test_path = "./test/functional_test/"
@@ -20,11 +21,16 @@ load_func_tests = do
 main :: IO ()
 main = do
     files <- load_func_tests
-    forM_ files $ \x -> do
-        handle <- openFile (func_test_path ++ x) ReadMode
-        content <- hGetContents handle
-        let r = runParser parse_CompUnit () x content
-        case r of
-            Right _ -> putStrLn $ "test '" ++ x ++ "' passed"
-            Left err -> putStrLn $ "test '" ++ x ++ "' failed with error: " ++ show err
-        hClose handle
+    codes <- forM files $ \x -> do
+        withFile (func_test_path ++ x) ReadMode $ \handle -> do
+            s <- hGetContents handle
+            pure $!! s -- must be strict
+    hspec $ func_tests (zip files codes)
+
+func_tests :: [(String, String)] -> Spec
+func_tests tests = do
+    describe "functional tests" $ do
+        forM_ tests $ \(test, code) -> do
+            it test $ do
+                let r = runParser parse_CompUnit () test code
+                r `shouldSatisfy` isRight
